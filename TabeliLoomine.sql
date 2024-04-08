42e75e83d30eba24580f6d013138383b5cb9c504
alexeypaal <120027728+alexeypaal@users.noreply.github.com>
2024-04-08 14:12:57 +0300

    Update TabeliLoomine.sql

--- TabeliLoomine.sql
+++ TabeliLoomine.sql
@@ -20,3 +20,12 @@ sisseastumis_kp date
 --primary key - primaarne võti mis annab unikaalsus
 --identity (1,1) - määrab igaühele oma nubri
 SELECT * FROM opilane;
+--andmete liisamine 
+INSERT INTO opilane(
+eesnimi, perenimi, isikukood, address, sisseastumis_kp)
+VALUES(
+'Alexey', 'Paal','50607220323', 'Tallinn','2023-08-16'),
+('Edu', 'Sulle','50607220321', 'Tallinn','2023-08-16'),
+('Karl', 'Õlu','50607220322', 'Tallinn','2023-08-16'),
+('Daniel', 'Sippo','50607220320', 'Tallinn','2023-08-16'),
+('Nikita', 'Karlos','50607220343', 'Tallinn','2023-08-16');
