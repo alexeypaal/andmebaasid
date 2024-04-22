e2c92e0e82225edc3abf41a7d4f41d3dd19bea3b
alexeypaal <120027728+alexeypaal@users.noreply.github.com>
2024-04-22 13:04:14 +0300

    tabeli film on loodus ja täidetud

--- protceduur.sql
+++ protceduur.sql
@@ -1,2 +1,64 @@
 CREATE DATABASE PaalProtceduur 
 USE PaalProtceduuur
+
+CREATE TABLE film(
+filmid int PRIMARY KEY identity (1,1),
+filmnimetus varchar(50),
+kestvus int,
+revisoor varchar(50),
+v_aasta int)
+select * from film 
+--mockaroo.com
+
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Tortilla Flat', 295, 'Artemis Avarne', 2005);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('11:14', 183, 'Faustine Pidgin', 2012);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Man Who Came to Dinner, The', 118, 'Alexis Lowre', 2009);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Tropical Malady (Sud pralad)', 285, 'Patrizia Laurens', 2010);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Philomena', 183, 'Nanice Barlace', 2007);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Inherit the Wind', 120, 'Halli Scohier', 2011);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Unstoppable', 134, 'Cyb Sillars', 2004);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Barbed Wire', 209, 'Brandon Hapke', 2007);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('There Be Dragons', 156, 'Dannie Cosslett', 2004);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Sweet November', 283, 'Cyrille Pieper', 2010);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Riot on Sunset Strip', 236, 'Cullan Shakespeare', 2004);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Three Came Home', 175, 'Shandee Fursland', 2008);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('13 Beloved (13 game sayawng)', 202, 'Brok Ronchetti', 1996);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Teddy Bears'' Picnic', 216, 'Vinnie Orbon', 2003);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Super Size Me', 216, 'Willi Leal', 1997);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Devil Commands, The', 202, 'Adler Chatwin', 1996);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Rio', 215, 'Joan Plenty', 1984);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Outside Providence', 225, 'Jerrome Sket', 1996);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Wild Child, The (Enfant sauvage, L'')', 287, 'Franchot Wheal', 1998);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('How to Steal a Million', 222, 'Solly Darlow', 2005);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('How to Survive a Plague', 120, 'Inger Tuhy', 2007);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('My Fake  Fiance', 274, 'Shir Dandy', 2010);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Stuck in Love', 235, 'Julia Rolland', 1979);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('If These Walls Could Talk', 224, 'Lanna Lates', 1997);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Rocco and His Brothers (Rocco e i suoi fratelli)', 281, 'Nananne Dooman', 1992);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Red Riding: 1974', 111, 'Sharon McVanamy', 1990);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Legend of Boggy Creek, The', 298, 'Graham Banister', 2010);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Cat''s Meow, The', 100, 'Royal Donaghie', 2004);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Striking Distance', 249, 'Ofella Tipton', 2010);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Hell in the Pacific', 264, 'Putnem Folomkin', 1996);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Mr. Sardonicus', 164, 'Sidonnie Shafe', 2008);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Waist Deep', 210, 'Vivi Happer', 1989);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Through and Through (Na wylot)', 135, 'Gabie Tudball', 2001);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('First Men in the Moon', 100, 'Hilary Kunat', 1998);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Hit, The', 135, 'Arnoldo Nouch', 2010);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Living in Emergency: Stories of Doctors Without Borders', 143, 'Burl Ellingsworth', 1985);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Expecting Love (Mala wielka milosc)', 242, 'Lester Skilbeck', 2010);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Ill Manors', 163, 'Carry Rumens', 2010);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Otello', 140, 'Alie Benoist', 1989);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Great Silence, The (Grande silenzio, Il)', 145, 'Maximilianus Eaklee', 2005);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Vitelloni, I (a.k.a. The Young and the Passionate)', 129, 'Kirstyn McMillian', 2009);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Dreamland', 100, 'Denni Toffolo', 1993);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Juliet of the Spirits (Giulietta degli spiriti)', 159, 'Germayne Mersey', 1998);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Bad Teacher', 209, 'Hyacinthie Ormonde', 1993);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Naked Fear', 241, 'Betteann Prickett', 2004);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Make It Happen', 286, 'Zora Base', 1999);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Liliom', 172, 'Carissa Dampney', 1998);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Trapped in the Closet: Chapters 1-12', 209, 'Chloris Lenox', 2003);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Sam Peckinpah''s West: Legacy of a Hollywood Renegade', 153, 'Gypsy Larkkem', 2013);
+insert into film (filmnimetus, kestvus, revisoor, v_aasta) values ('Vikings, The', 300, 'Sax Shellcross', 2005);
+
+select * from film;
