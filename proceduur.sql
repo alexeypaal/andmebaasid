6f66d09911986f82c38aeef8911375290aad5147
alexeypaal <120027728+alexeypaal@users.noreply.github.com>
2024-04-24 12:13:07 +0300

    proceduur otsing1taht

--- proceduur.sql
+++ proceduur.sql
@@ -4,3 +4,8 @@ filmNimetus varchar(50),
 kestvus int,
 rezisoor varchar(50),
 v_aasta int);
+  
+  CREATE PROCEDURE `otsing1taht`(IN `taht` CHAR(1)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER begin
+	SELECT * FROM FILM
+	WHERE filmNimetus LIKE CONCAT(taht,'%');
+end;
